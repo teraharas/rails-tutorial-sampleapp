9b6c0ecc3d43a34518b8130c90f3da81f386e52a
teraharas <triple.zero.option@gmail.com>
2015-12-27 15:38:13 +0000

    Patch

--- app/models/user.rb
+++ app/models/user.rb
@@ -79,10 +79,7 @@ class User < ActiveRecord::Base
   end
   
   def feed
-    following_ids = "SELECT followed_id FROM relationships
-                     WHERE  follower_id = :user_id"
-    Micropost.where("user_id IN (#{following_ids})
-                     OR user_id = :user_id", user_id: id)
+    Micropost.all
   end
   
   # ユーザーをフォローする
