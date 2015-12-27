90812c79ed302f631b019c531f1c9f6eca6cdd96
teraharas <triple.zero.option@gmail.com>
2015-12-27 10:56:57 +0000

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  has_many :microposts, dependent: :destroy
+  
   attr_accessor :remember_token, :activation_token
   before_save   :downcase_email
   before_create :create_activation_digest
@@ -66,6 +68,10 @@ class User < ActiveRecord::Base
   # パスワード再設定の期限が切れている場合はtrueを返す
   def password_reset_expired?
     reset_sent_at < 2.hours.ago
+  end
+  
+  def feed
+    Micropost.where("user_id = ?", id)
   end
   
   private
